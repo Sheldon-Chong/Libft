672a9efa58fb423ed05246bb5099126ce4d90963
Sheldon Chong <shechong@u81z01s03.42kl.edu.my>
2023-05-09 11:15:26 +0800

    test

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shechong <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 11:33:06 by shechong          #+#    #+#             */
+/*   Updated: 2023/04/30 11:33:07 by shechong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
---
+++ ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shechong <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 11:53:32 by shechong          #+#    #+#             */
+/*   Updated: 2023/04/30 11:53:37 by shechong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s || !str)
+		return (NULL);
+	if ((size_t)start >= ft_strlen(s))
+		return (ft_strdup(""));
+	i = 0;
+	while (s[i + start] && i < len)
+	{
+		str[i] = s[i + start];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
